d6a7ff17c7e21bbf9dc66af4849287ccad1d609b
mitsu-ksgr <k.sgr.mitsu.g@gmail.com>
2025-03-02 07:36:08 +0900

    wip: target

--- README.md
+++ README.md
@@ -4,3 +4,20 @@ Dirback
 - A simple directory backup tool.
 
 
+## Notes
+### Base path
+- `APP_DATA_PATH`
+  - Linux: "~/.local/share/dirback"
+  - Windows: "TODO"
+
+### Data path
+- Target path
+  - `/path/to/target/dir`
+- Application data path
+  - `${APP_DATA_PATH}/targets.json`
+    - target id (uuid?)
+    - target dir path
+- BackupFile path
+  - `${APP_DATA_PATH}/targets/{TARGET_ID}/info.json`
+  - `${APP_DATA_PATH}/targets/{TARGET_ID}/backups/{DATETIME}.tar.gz`
+
--- crates/dirback/src/domain/model/target.rs
+++ crates/dirback/src/domain/model/target.rs
@@ -4,20 +4,34 @@
 //! Backup target.
 //! Contains the target directory path and id.
 //!
+//!
+
+use crate::domain::model::backup_file::BackupFile;
 
 #[derive(Debug)]
 pub struct Target {
+    /// Target ID.
+    ///
+    /// Used as an id when saving backups.
     pub id: String,
-    pub path: String,
-    // TODO backupfile list
+
+    /// Path to the target dir.
+    pub path: PathBuf,
+
+    /// Backup files.
+    pub backups: Vec<BackupFile>,
 }
 
 impl Target {
     /// Create a new BackupTarget.
     ///
     /// The id is the identifier of target.
-    pub fn new(id: String, path: String) -> Self {
-        Target { id, path }
+    pub fn new(id: String, target_dir_path: &Path) -> Self {
+        Target {
+            id,
+            path: target_dir_path.to_path_buf(),
+            backups: Vec::<BackupFile>::new(),
+        }
     }
 }
 
