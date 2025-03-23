40e11cab93f99b527f24536053b84505cc938f3c
mitsu-ksgr <k.sgr.mitsu.g@gmail.com>
2025-03-24 03:48:00 +0900

    update jig/run-cmd.sh

--- jig/run-cmd.sh
+++ jig/run-cmd.sh
@@ -2,7 +2,11 @@
 #
 # Run cmd crate with DIRBACK_STORE_DIR env var.
 #
-readonly APP_DATA_PATH="$(dirname $0)/../tmp/cmd-test"
+
+# move to the project root.
+cd $(dirname $0)/..
+
+readonly APP_DATA_PATH="./tmp/cmd-test"
 
 if [ ! -d "${APP_DATA_PATH}" ]; then
     mkdir -p "${APP_DATA_PATH}"
