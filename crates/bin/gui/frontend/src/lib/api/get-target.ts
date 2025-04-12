a7aef0afdfa6fa4021bdb4d318b416d6a36999db
mitsu-ksgr <k.sgr.mitsu.g@gmail.com>
2025-04-12 19:32:52 +0900

    gui: fmt

--- crates/bin/gui/frontend/src/lib/api/get-target.ts
+++ crates/bin/gui/frontend/src/lib/api/get-target.ts
@@ -7,10 +7,10 @@ import type { Command } from "./types/command";
 import type { Target } from "./types/target";
 
 export async function getTarget(target_id: string): Promise<Target | null> {
-    return await dispatch({
-        type: "GetTarget",
-        payload: {
-          target_id,
-        },
-    });
+  return await dispatch({
+    type: "GetTarget",
+    payload: {
+      target_id,
+    },
+  });
 }
